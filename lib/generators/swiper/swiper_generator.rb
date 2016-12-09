9fb4f17dda8e4d0c971045de0458d559813235d6
nsei <nsevinctekin@gmail.com>
2016-12-09 14:41:02 +0200

    add to scripts

--- lib/generators/swiper/swiper_generator.rb
+++ lib/generators/swiper/swiper_generator.rb
@@ -4,6 +4,10 @@ module Swiper
 
       source_root File.expand_path("../../../../app", __FILE__)
       desc 'Copies Swiper stylesheet to your application.'
+      
+      def generate_scripts
+        copy_file "assets/javascripts/swiper.js", "app/assets/javascripts/swiper.js"
+      end
 
       def generate_styles
         copy_file "assets/stylesheets/swiper.css", "app/assets/stylesheets/swiper.css"
